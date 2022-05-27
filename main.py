b20f0b99ec7889123c8bd240c78aec6336b6dab7
PPedroso <pedroso_pedro@hotmail.com>
2022-05-27 23:47:52 +0100

    Add menu interface

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print('hello world')
+from dog_api import *
+import os
+
+user_input = ''
+
+while True:
+    os.system('cls')
+    print('1 - Get all breeds')
+    print('0 - Quit')
+
+    user_input = input()
+
+    if user_input == '1':
+        breeds = get_all_breeds()
+
+        for breed in breeds:
+            print(breed)
+    
+    if user_input == '0':
+        break
+    
+    input("Press any key to continue")    
+    

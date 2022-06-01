24630b735f111581c3034ba152cd190b33c87abc
PPedroso <pedroso_pedro@hotmail.com>
2022-06-01 06:47:10 +0100

    Add option to get dog by breed and random image

--- dog_api.py
+++ dog_api.py
@@ -2,14 +2,35 @@ import requests as r
 import json as j
 
 
-url = 'https://dog.ceo/api/breeds'
-get_all = '/list/all'
+class endpoints:
+    url = 'https://dog.ceo/api'
+    get_all = '/breeds/list/all'
+    get_by_breed =  '/breed/<breed>/images'
+    get_random_image = '/breeds/image/random'
+
+def get(url):
+    return r.get(url).json()['message']
 
 def get_all_breeds():
-    res = r.get(url+get_all).json()['message']
+    res = get(endpoints.url+endpoints.get_all)
 
     breeds= []
     for b in res:
         breeds += [b]
 
     return breeds
+
+def get_images_by_breed(breed):
+    res = get(endpoints.url + endpoints.get_by_breed.replace('<breed>',breed))
+
+    images = []
+    for image in res:
+        images += [image]
+
+    return images
+
+def get_random_image():
+    res = get(endpoints.url + endpoints.get_random_image)
+
+    return res
+
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ user_input = ''
 while True:
     os.system('cls')
     print('1 - Get all breeds')
+    print('2 - Get images by breed')
+    print('3 - Get random dog image')
     print('0 - Quit')
 
     user_input = input()
@@ -14,8 +16,21 @@ while True:
         breeds = get_all_breeds()
 
         for breed in breeds:
-            print(breed)
+            print(breed, end = ' ')
+        print()
     
+    if user_input == '2':
+        breed = input("Iput the breed to print\n")
+        
+        dog_images = get_images_by_breed(breed)
+
+        for image in dog_images:
+            print(image)
+
+    if user_input == '3':
+        random_image = get_random_image()
+        print(random_image)
+
     if user_input == '0':
         break
     

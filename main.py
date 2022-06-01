17093bba57c55045f66a94bd9048c3fc0219e369
PPedroso <pedroso_pedro@hotmail.com>
2022-06-01 23:18:31 +0100

    Add endpoint for getting all breeds and getting images based on breed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from socket import SocketIO
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, send
 from dog_api import *
 import os
@@ -11,15 +11,22 @@ app.config['SECRET'] = 'secret123'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-@socketio.on('all_breeds')
-def get_all_breeds():
-    breeds = get_all_breeds()
+@app.route('/images_by_breed')
+def images_by_breed():
+    breed = request.args.get('breed')
+        
+    dog_images = get_images_by_breed(breed)
 
-    return breeds
+    return render_template('/index.html', data = {'dog_images' : dog_images })
+
+@app.route('/all_breeds')
+def all_breeds():
+    breeds = get_all_breeds()
+    return render_template('/index.html', data = {'breeds' : breeds})
 
 @app.route('/')
 def index():
-    return render_template('/index.html')
+    return render_template('/index.html', data = {})
 
 
 if __name__ == "__main__":
